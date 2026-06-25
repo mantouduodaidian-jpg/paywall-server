b960e9b2d59f19998a868b7550b7f02377e04f71
user <user@example.com>
2026-06-25 19:13:28 +0800

    fix: remove service worker + unregister

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'linjian-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/wallpaper.html','/chat.html','/expense.html','/favicon.svg','/manifest.json'])));
-  self.skipWaiting();
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
